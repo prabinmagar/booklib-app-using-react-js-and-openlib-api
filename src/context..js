392a117a5653272f574c68558564e99635cf567c
rithh <rithtkd@gmail.com>
2023-07-08 23:11:06 +0700

    first and last commit

--- src/context..js
+++ src/context..js
@@ -1,68 +1,105 @@
-import React, {useState, useContext, useEffect} from 'react';
-import { useCallback } from 'react';
-const URL = "http://openlibrary.org/search.json?title=";
-const AppContext = React.createContext();
+import React, { useState, useContext, useEffect } from 'react'
+import { useCallback } from 'react'
+const URL = 'http://openlibrary.org/search.json?title='
+const URLSI = 'https://openlibrary.org/search/inside.json?q='
+const AppContext = React.createContext()
 
-const AppProvider = ({children}) => {
-    const [searchTerm, setSearchTerm] = useState("the lost world");
-    const [books, setBooks] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [resultTitle, setResultTitle] = useState("");
+const AppProvider = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [resultTitle, setResultTitle] = useState('')
 
-    const fetchBooks = useCallback(async() => {
-        setLoading(true);
-        try{
-            const response = await fetch(`${URL}${searchTerm}`);
-            const data = await response.json();
-            const {docs} = data;
+  const searchText = useCallback(async () => {
+    setLoading(true)
+    try {
+      const newResponse = await fetch(`${URLSI}${searchTerm}`)
+      const newData = await newResponse.json()
+      const { hits } = newData.hits
+      if (hits) {
+        const getMatch = hits.slice(0, 5)
+        const { edition } = getMatch[0]
+        const match = edition.key
+        window.open(`https://openlibrary.org${match}`)
+      } else {
+        setResultTitle('No Search Result Found!')
+      }
+      setLoading(false)
+      setSearchTerm('')
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }, [searchTerm])
 
-            if(docs){
-                const newBooks = docs.slice(0, 20).map((bookSingle) => {
-                    const {key, author_name, cover_i, edition_count, first_publish_year, title} = bookSingle;
+  const fetchBooks = useCallback(async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(`${URL}${searchTerm}`)
+      const data = await response.json()
+      const { docs } = data
 
-                    return {
-                        id: key,
-                        author: author_name,
-                        cover_id: cover_i,
-                        edition_count: edition_count,
-                        first_publish_year: first_publish_year,
-                        title: title
-                    }
-                });
+      if (docs) {
+        const newBooks = docs.slice(0, 20).map(bookSingle => {
+          const {
+            key,
+            author_name,
+            cover_i,
+            edition_count,
+            first_publish_year,
+            title
+          } = bookSingle
 
-                setBooks(newBooks);
+          return {
+            id: key,
+            author: author_name,
+            cover_id: cover_i,
+            edition_count: edition_count,
+            first_publish_year: first_publish_year,
+            title: title
+          }
+        })
 
-                if(newBooks.length > 1){
-                    setResultTitle("Your Search Result");
-                } else {
-                    setResultTitle("No Search Result Found!")
-                }
-            } else {
-                setBooks([]);
-                setResultTitle("No Search Result Found!");
-            }
-            setLoading(false);
-        } catch(error){
-            console.log(error);
-            setLoading(false);
+        setBooks(newBooks)
+
+        if (newBooks.length > 1) {
+          setResultTitle('Your Search Result')
+        } else {
+          setResultTitle('No Search Result Found!')
         }
-    }, [searchTerm]);
+      } else {
+        setBooks([])
+        setResultTitle('No Search Result Found!')
+      }
+      setSearchTerm('')
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }, [searchTerm])
 
-    useEffect(() => {
-        fetchBooks();
-    }, [searchTerm, fetchBooks]);
+  useEffect(() => {
+    searchText()
+  }, [searchTerm, searchText])
 
-    return (
-        <AppContext.Provider value = {{
-            loading, books, setSearchTerm, resultTitle, setResultTitle,
-        }}>
-            {children}
-        </AppContext.Provider>
-    )
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        books,
+        setSearchTerm,
+        resultTitle,
+        setResultTitle
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 
 export const useGlobalContext = () => {
-    return useContext(AppContext);
+  return useContext(AppContext)
 }
 
-export {AppContext, AppProvider};
+export { AppContext, AppProvider }
